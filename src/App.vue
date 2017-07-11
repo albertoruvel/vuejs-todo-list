337ededf3bc93225fdd489902d5ff922ea8dc7c0
Alberto <arubalcaba@24hourfit.com>
2017-07-11 16:17:17 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add="addTodo"></create-todo>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import CreateTodo from '@/components/CreateTodo'
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo
+  },
+  // data function avails data to the template
+  data () {
+    return {
+      todos: [{
+        title: 'Project one',
+        description: 'description one',
+        done: false
+      }, {
+        title: 'Project two',
+        description: 'description two',
+        done: false
+      }, {
+        title: 'Project three',
+        description: 'description three',
+        done: true
+      }, {
+        title: 'Project four',
+        description: 'description four',
+        done: false
+      }]
+    }
+  },
+  methods: {
+    addTodo (title) {
+      this.todos.push({
+        title,
+        done: false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+var $ = require('jquery')
+window.jQuery = $
+
+require('../static/semantic/dist/semantic.js')
+require('../static/semantic/dist/semantic.css')
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
